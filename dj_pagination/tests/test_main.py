1f26a2975ddc694b055485465a679f90d5794d86
Alexandre Muller <alexandre.muller@polyconseil.fr>
2023-12-20 15:49:20 +0100

    tests: add mandatory get_response argument for middleware

--- dj_pagination/tests/test_main.py
+++ dj_pagination/tests/test_main.py
@@ -29,6 +29,8 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from unittest import mock
+
 from django.core.paginator import Paginator
 from django.http import HttpRequest as DjangoHttpRequest
 from django.template import Template, Context
@@ -424,7 +426,7 @@ class MiddlewareTestCase(SimpleTestCase):
     """
 
     def test_get_page_in_request(self):
-        middleware = PaginationMiddleware()
+        middleware = PaginationMiddleware(get_response=mock.MagicMock())
         request = DjangoHttpRequest()
         middleware.process_request(request)
         self.assertEqual(request.page(""), 1)
