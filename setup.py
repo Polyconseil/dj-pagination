1f752a10e42950e2cec89ed770edadd212198540
Alexandre Muller <alexandre.muller@polyconseil.fr>
2024-01-11 09:34:30 +0100

    setup: add support for Python 3.11 and 3.12

--- setup.py
+++ setup.py
@@ -78,6 +78,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     install_requires=["Django<5.1"],
     include_package_data=True,
